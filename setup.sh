5f7efa7734cd26607b0ee90376f1f2a08855f37e
eliacortesi <eliacortesi278@gmail.com>
2024-04-30 21:31:02 +0200

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #!bin/bash
 #
-# This script is used to setup all the programs I use
+# This script is used to setup a good part of my system
+
+echo "Adding multilib support..."
+sudo sed -i '92s/#//' /etc/pacman.conf
+sudo sed -i '93s/#//' /etc/pacman.conf
+
+echo "Updating system and installing programs..."
 sudo pacman -Syu --needed --noconfirm \
 	alacritty\
 	amd-ucode\
+	base-devel\
 	cups\
 	curl\
 	docker\
@@ -30,6 +37,7 @@ sudo pacman -Syu --needed --noconfirm \
 	redshift\
 	rofi\
 	rsync\
+	samba\
 	scrot\
 	steam\
 	stow\
@@ -39,6 +47,14 @@ sudo pacman -Syu --needed --noconfirm \
 	wget\
 	wine\
 	zip\
+
+echo "Installing yay"
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+cd ..
+rm -rf yay
+echo "yay installed"
 
 yay -S --needed --noconfirm \
 	drawio-desktop\

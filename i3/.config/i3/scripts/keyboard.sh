b0de0f2967d28e090874169f16739eb16c127648
eliacortesi <eliacortesi278@gmail.com>
2024-05-07 16:09:32 +0200

    update

--- i3/.config/i3/scripts/keyboard.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-#
-# This script is used to change the keyboard layout to US when connected to HDMI
-if xrandr | grep -q "HDMI-A-0 connected"; then
-	setxkbmap us
-else
-	setxkbmap it
-fi
